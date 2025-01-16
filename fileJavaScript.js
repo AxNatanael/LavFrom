397025cf28c55d15967af08ed3052b19c5e68e11
Natanael Alexi <alexinatanael04@gmail.com>
2025-01-16 10:53:08 +0100

    Robetta nuova più nuova della vecchia nuova

--- fileJavaScript.js
+++ fileJavaScript.js
@@ -13,4 +13,35 @@ function ConfButton(){
     var fullUrl=url + "?Nome=" + name + "&Cognome=" + Cognome;
     console.log(fullUrl);
     faiGet(fullUrl);
+    document.getElementById("BNome").value = "";
+    document.getElementById("BCognome").value = "";
+}
+
+function salvaLocal(){
+    
+    var nome = document.getElementById("CNome").value;
+    var cognome = document.getElementById("CCognome").value;
+    
+    localStorage.setItem("Nome", nome);
+    localStorage.setItem("Surname", cognome);
+    
+    console.log(nome + " " + cognome);
+
+    document.getElementById("CNome").value = "";
+    document.getElementById("CCognome").value = "";
+}
+
+function welcome(){
+    
+    var nomew = localStorage.getItem("Nome") || ""; 
+    var cognomew = localStorage.getItem("Surname") || "" ; 
+    if (nomew == "" && cognomew == "") {
+        alert("Benvenuto");
+    }
+    else
+    {
+        alert(nomew + cognomew + " was here");
+    }
+    console.log(nomew + " " + cognomew);
+
 }
